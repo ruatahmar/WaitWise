0b917225603b7f1178fc62972216177d49dd3765
Ruata Hmar <hvanlalruata511@gmail.com>
2026-02-10 20:04:11 +0530

    frontend moved

--- client/src/api/authApi.js
+++
@@ -1,7 +0,0 @@
-import { api, publicApi } from "../utils/apiClient";
-
-export const login = (data) => api.post("/auth/login", data);
-export const register = (data) => api.post("/auth/register", data);
-export const refreshToken = () => publicApi.post("/auth/refresh");
-export const logout = () => api.post("/auth/logout");
-export const logoutAllDevices = () => api.post("/auth/logoutAll")
